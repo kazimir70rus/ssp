12c3e2e5256d68fc9b482c5ecff2c556383285c4
kazimir70rus <kazzzimir@gmail.com>
2020-02-01 00:13:52 +0700

    накидал макет таблицы по документу ССП2 ТОЯН.xls

--- ssp/views/index.php
+++ ssp/views/index.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width">
     <link href="<?=BASE_URL?>css/main.css" rel="stylesheet" type="text/css">
 
-    <!--    убрал, так как все это от лукавого
+<!--    убрал, так как все это от лукавого... Саша прости.... вернул )  -->
 <style>
     table {
         width: 100%; /* Ширина таблицы */
     }
     th {
-        background: maroon; /* Цвет фона ячеек */
+        background: green; /* Цвет фона ячеек */
         color: white; /* Цвет текста */
     }
     
     </style>    
- --> 
+<!-- --> 
+
 </head>
 <body>
 
@@ -39,17 +40,33 @@
 
     <table border="0">
         <tr>
-            <th>Сделать</th>
-            <th>Исполнитель</th>
+            <th>№</th>
             <th>Дата исполнения</th>
+            <th>Дата начала</th>
+            <th>Задание</th>
+            <th>Ответственный</th>
+            <th>Вид результата</th>
+            <th>Вид отчета</th>
+            <th>Потребитель</th>
+            <th>Контролер</th>
+            <th>Результат</th>
         </tr>
 
         <?php
+            $i = 0;
             foreach ($author_tasks as $one_task) {
+                $i++;
                 echo '<tr>';
+                echo '<td>', $i, '</td>';
+                echo '<td>', $one_task['data_end'], '</td>';
+                echo '<td></td>';
                 echo '<td>', $one_task['name'], '</td>';
                 echo '<td>', $one_task['fio_executor'], '</td>';
-                echo '<td>', $one_task['data_end'], '</td>';
+                echo '<td></td>';
+                echo '<td></td>';
+                echo '<td></td>';
+                echo '<td></td>';
+                echo '<td></td>';
                 echo '</tr>';
         }
         ?>
