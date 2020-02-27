34b2244f4c7de6e720739f04f0d6a359f4b4a8aa
kazimir70rus <kazzzimir@gmail.com>
2020-02-27 13:19:54 +0700

    подготовили макет

--- views/index.php
+++ views/index.php
@@ -14,7 +14,23 @@
         background: green; /* Цвет фона ячеек */
         color: white; /* Цвет текста */
     }
-    
+
+    .container {
+      display: flex;
+    }
+    .item {
+        width: 49%;
+    }
+
+    @media screen and (max-width: 500px) {
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .item {
+            width: 99%;
+        }
+    }    
     </style>    
 <!-- --> 
 
@@ -27,6 +43,25 @@
     <a href="<?=BASE_URL?>add_task">Создать задачу</a>
     
     <br>
+    
+    <div class="container">
+        <div class="item" style="border: 1px solid red">
+        12
+        </div>
+        <div class="item" style="border: 1px solid green">
+        12
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="item" style="border: 1px solid blue">
+        12
+        </div>
+        <div class="item" style="border: 1px solid black">
+        12
+        </div>
+    </div>
+    
     <p><b>Список задач к исполнению:</b></p>
    <?php
         foreach ($list_tasks as $one_task) {
@@ -40,32 +75,17 @@
 
     <table border="0">
         <tr>
-            <th>№</th>
             <th>Дата исполнения</th>
-            <th>Дата начала</th>
             <th>Задание</th>
-            <th>Ответственный</th>
-            <th>Вид результата</th>
-            <th>Вид отчета</th>
-            <th>Потребитель</th>
-            <th>Контролер</th>
             <th>Результат</th>
         </tr>
 
         <?php
             $i = 0;
             foreach ($author_tasks as $one_task) {
-                $i++;
                 echo '<tr>';
-                echo '<td>', $i, '</td>';
                 echo '<td>', $one_task['data_end'], '</td>';
-                echo '<td></td>';
                 echo '<td>', $one_task['name'], '</td>';
-                echo '<td>', $one_task['fio_executor'], '</td>';
-                echo '<td></td>';
-                echo '<td></td>';
-                echo '<td></td>';
-                echo '<td></td>';
                 echo '<td></td>';
                 echo '</tr>';
         }
