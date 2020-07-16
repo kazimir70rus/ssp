cbf513686e0c881e993bc692a4b09c537a8544af
kazimir70rus <kazzzimir@gmail.com>
2020-07-16 08:47:14 +0700

    попытка сделать админку

---
+++ views/admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Wellcom</title>
+</head>
+
+<body>
+
+<h2>Страница для Админа</h2>
+
+<div><a href="<?=BASE_URL?>exit">Выход</a></div>
+
+</body>
+</html>
