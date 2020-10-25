3f1df1f4194d287c8920424b90946d8cb9886430
novokhatsky <program_maker@mail.ru>
2020-10-25 23:54:50 +0700

    уточнение действий над задачей

--- models/task.php
+++ models/task.php
@@ -364,9 +364,35 @@ Class Task
                     ->db
                     ->getList($query, ['id_task' => $id_task, 'id_user' => $id_user]);
 
+        // если у задачи инициатор и потребитель одно лицо, то контроллер может подтвердить выполнение
+        $query = '
+            select
+                count(distinct id_user) as cnt,
+                (select id_user from task_users where id_task = :id_task and id_tip = 4) as id_controller
+            from
+                tasks join task_users using (id_task)
+            where
+                id_task = :id_task 
+                and id_tip in (2, 3)
+                and id_condition = 3
+        ';
+
+        $data = $this->db->getRow($query, ['id_task' => $id_task]);
+
+        if (((int)$data['cnt'] == 1) && ($data['id_controller'] == $id_user)) {
+            // инициатор и потребитель одно лицо
+            // добавляем действие
+            $result[] = [
+                'id_action' => 13,
+                'name' => 'подтвердить выполнение',
+                'need_dt' => 0,
+                'change_penalty' => 0,
+            ];
+        }
+
         // для выполнения некоторых действий нужно выполнение определенных условий
         foreach ($result as $index => $action) {
-            if ((int)$action['id_action'] == 12) {
+            if (isset($action['id_action']) && ((int)$action['id_action'] == 12)) {
 
                 // проверим тип отчета у данной задачи, требуются ли файлы
                 $query = 'select need_file from tasks join type_report using (id_report) where id_task = :id_task';
